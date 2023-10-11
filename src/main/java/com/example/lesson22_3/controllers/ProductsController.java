7771ab907f0ca0c27b144906dec59d61716c4962
asrytikov <asrytikov@gmail.com>
2023-10-11 21:33:39 +0400

    Lesson22

--- src/main/java/com/example/lesson22_3/controllers/ProductsController.java
+++ src/main/java/com/example/lesson22_3/controllers/ProductsController.java
@@ -1,2 +1,24 @@
-package com.example.lesson22_3.controllers;public class ProductsController {
+package com.example.lesson22_3.controllers;
+
+import com.example.lesson22_3.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductsController {
+
+    private final ProductService productService;
+
+    public ProductsController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/products")
+    public String viewProducts(Model model){
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+        return "products.html";
+    }
+
 }
--- src/main/java/com/example/lesson22_3/service/ProductService.java
+++ src/main/java/com/example/lesson22_3/service/ProductService.java
@@ -1,2 +1,22 @@
-package com.example.lesson22_3.service;public class ProductService {
+package com.example.lesson22_3.service;
+
+import com.example.lesson22_3.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product p){
+        products.add(p);
+    }
+
+    public List<Product> findAll(){
+        return products;
+    }
+
 }
